c84433d1ec0f809fa2cf7f511669a1a71853616e
noorzai <fardin.noorzai321@gmail.com>
2025-03-19 13:40:16 +0430

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+require('dotenv').config();
+const express = require('express');
+const mongoose = require('mongoose');
+const cors = require('cors');
+
+
+const app = express();
+app.use(express.json());
+const logger = (req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.method} ${req.url}`);
+    console.log('Request Body:');
+    for (const [key, value] of Object.entries(req.body)) {
+        console.log(`  ${key}: ${value}`);
+    }
+    next();
+};
+app.use(logger)
+app.use(cors());
+
+mongoose.connect(process.env.MONGO_URI, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+}).then(() => {
+    console.log('Connected to MongoDB');
+}).catch((err) => {
+    console.log('Error connecting to MongoDB', err);
+});
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the my simple crude api');
+});
+
+const userRoutes = require('./routes/user');
+app.use('/users', userRoutes);
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
